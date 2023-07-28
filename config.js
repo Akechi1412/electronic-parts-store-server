0af4e969820ccb0a06986299e719220690a07cc4
Akechi1412 <nguyenphong10042002@gmai.com>
2023-07-28 10:58:59 +0700

    update config.js

--- config.js
+++ config.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const config = {
   db: {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.MYSQL_DB,
